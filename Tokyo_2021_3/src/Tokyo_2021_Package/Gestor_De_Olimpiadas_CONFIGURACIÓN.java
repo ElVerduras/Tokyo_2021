93789e90482cbabca85ccf874d07fa4e55a35ca4
diegopila99 <84112695+DIEGOPILA99@users.noreply.github.com>
2021-11-11 18:13:04 -0300

    colooo

--- Tokyo_2021_3/src/Tokyo_2021_Package/Gestor_De_Olimpiadas_CONFIGURACIÓN.java
+++ Tokyo_2021_3/src/Tokyo_2021_Package/Gestor_De_Olimpiadas_CONFIGURACIÓN.java
@@ -1,44 +1,70 @@
 package Tokyo_2021_Package;
 import javax.swing.*;
-import java.awt.*;  
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;  
 
 @SuppressWarnings("serial")
 public class Gestor_De_Olimpiadas_CONFIGURACIÓN extends Frame{
-	 private static Label Label_User; 
-	 private static TextField Text_User;  
-	 private static Label Label_Password;   
-	 private static TextField Text_Password ;  
-	 private JButton Return;  
- 	
-	public Gestor_De_Olimpiadas_CONFIGURACIÓN() {
-		
-		setLayout(new FlowLayout());
-		
-		Label_User = new Label("USUARIO DB      ");
-		Text_User  = new TextField("", 10); 
-		Text_User.setEditable(true); 
-		
-		add(Label_User); 
-		add(Text_User); 
-		
-		Label_Password = new Label("PASSWORD DB"); 
-		Text_Password  = new TextField("", 10); 
-		Text_Password.setEditable(true); 
-		
-		add(Label_Password); 
-		add(Text_Password); 
-		
-		Return = new JButton("VOLVER");
-		add(Return);
-		setTitle(" Gestor De Olimpiadas - CONFIGURACIÓN ");
-		setSize(250, 100);
-		setVisible(true); 
-	}
+	 private static JLabel Label_User; 
+	 private static JTextField Text_User;  
+	 private static JLabel Label_Password;   
+	 private static JTextField Text_Password ;  
+	 private static JButton Return_;  
+ 			public static void create(JFrame frame) {
+
+			//DECLARATION USER
+			Label_User = new JLabel("      USUARIO DB");
+			Label_User.setBounds(20,20,100,27);
+			Label_User.setVerticalTextPosition(SwingConstants.BOTTOM);
+			Label_User.setHorizontalTextPosition(SwingConstants.CENTER);
 	
-	public static void main(String[] args) {
-		Gestor_De_Olimpiadas_CONFIGURACIÓN Window_ = new Gestor_De_Olimpiadas_CONFIGURACIÓN();
-	}
-}
+			Text_User  = new JTextField("", 10); 
+			Text_User.setBounds(115,20,140,25);
+			Text_User.setEditable(true); 
+			
+	        frame.getContentPane().add(Label_User);
+	        frame.getContentPane().add(Text_User);
+			//DECLARATION PASSWORD
+			Label_Password = new JLabel("PASSWORD DB"); 
+			Label_Password.setBounds(20,40,100,47);
+	
+			Text_Password  = new JTextField("", 10); 
+			Text_Password.setBounds(115,50,140,25);
+	
+			Text_Password.setEditable(true); 
+	        frame.getContentPane().add(Label_Password);
+	        frame.getContentPane().add(Text_Password);
+	
+	 
+			//CREATE UP RETURN			
+			Return_ = new JButton("VOLVER");
+			Return_.setBounds(90,80,100,30);
+	        frame.getContentPane().add(Return_);
+			//BEGINNING SET UP RETURN
+	        Return_.addActionListener( new ActionListener(){
+	        	@Override
+	 			 public void actionPerformed(ActionEvent e) {
+	        		//BEGINNING SET UP ACCION´S 
+	        		//MODIFICATION BBDD
+	    	        //END  SET UP ACCION´S 
+	        		frame.dispose();
+				 }
+	 		 }); 
+	        //END  SET UP RETURN
+	     }
+ 
+	
+		public static void createWindow() {
+			JFrame Window = new JFrame("Gestor de Olimpiadas - CONFIGURACIÓN ");
+			Window.setBounds(100, 100, 300, 170);
+			Window.getContentPane().setLayout(null);
+			create(Window);
+			Window.setResizable(false);
+			Window.setVisible(true);
+		}
 
-
-
+		public static void main(String[] args) {
+			createWindow();
+		}	
+	}	
--- Tokyo_2021_3/src/Tokyo_2021_Package/Gestor_De_Olimpiadas_DEPORTISTAS.java
+++ Tokyo_2021_3/src/Tokyo_2021_Package/Gestor_De_Olimpiadas_DEPORTISTAS.java
@@ -1,18 +1,78 @@
 package Tokyo_2021_Package;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;  
 import javax.swing.*;
-import javax.swing.JFrame;
 
 public class Gestor_De_Olimpiadas_DEPORTISTAS {
-
+	private JTable Table = new JTable();
+  	private JPanel Panel_Buttons = new JPanel();
+	//private String[] Title_Table2 = {"Nombre y Apellido","País","Disciplina"};
+	
 	public static void create(JFrame frame) {
+        //BUTTON NEW
+		JButton Button_New = new JButton("NUEVO");
+		Button_New.setBounds(160,20,100,50);
+        Button_New.setVerticalTextPosition(SwingConstants.BOTTOM);
+        Button_New.setHorizontalTextPosition(SwingConstants.CENTER);
+        frame.getContentPane().add(Button_New);
+			      //BEGINNING SET UP RETURN
+			        Button_New.addActionListener( new ActionListener(){
+			        	@Override
+			 			 public void actionPerformed(ActionEvent e) {
+			        		Gestor_De_Olimpiadas_NUEVO_DEPORTISTA.createWindow();
+						 }
+			 		 }); 
+			        //END  SET UP RETURN
+        //EXPORT CSV
+        JButton Button_Export = new JButton("EXPORTAR CSV");
+        Button_Export.setBounds(280,20,140,50);
+        Button_Export.setVerticalTextPosition(SwingConstants.BOTTOM);
+        Button_Export.setHorizontalTextPosition(SwingConstants.CENTER);
+        frame.getContentPane().add(Button_Export);
+        //VOLVER
+        JButton Button_Return = new JButton("VOLVER");
+		Button_Return.setBounds(440,20,100,50);
+		Button_Return.setVerticalTextPosition(SwingConstants.BOTTOM);
+		Button_Return.setHorizontalTextPosition(SwingConstants.CENTER);
+        frame.getContentPane().add(Button_Return); 
+        Button_Return.addActionListener( new ActionListener(){
+				        	@Override
+				 			 public void actionPerformed(ActionEvent e) {
+				        		//BEGINNING SET UP ACCION´S 
+				        		//MODIFICATION BBDD
+				    	        //END  SET UP ACCION´S 
+				        		frame.dispose();
+							 }
+				 		 }); 
+				        //END  SET UP RETURN
+        //BARRA DE TITULO DEPORTISTAS
+        JLabel Title_Table = new JLabel(" Deportistas ");
+        Title_Table.setBounds(160,20,360,75);
+        Title_Table.setVerticalTextPosition(SwingConstants.BOTTOM);
+        Title_Table.setHorizontalTextPosition(SwingConstants.CENTER);
+        frame.getContentPane().add(Title_Table);
+        //VISUALIZACION DE TABLA DEPORTISTAS
 	}
+	
+
+	public String[] Listado(){
+		String[] Cadena = null;
+		 
+	return Cadena;}
+	
 	public static void createWindow() {
-		JFrame Window = new JFrame("Gestor de Olimpiadas - DEPORTISTAS");
-		Window.setBounds(100, 100, 520, 300);
+		JFrame Window = new JFrame("Gestor de Olimpiadas - DEPORTISTAS ");
+		Window.setBounds(100, 100, 700, 500);
 		Window.getContentPane().setLayout(null);
 		create(Window);
 		Window.setResizable(false);
 		Window.setVisible(true);
-}
+	}
+}	
 	
-}
+
+
